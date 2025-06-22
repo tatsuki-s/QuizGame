e64a660cde9043334b39cd1f1ae9b1f9f4036c75
tatsuki-s <zekoushn@gmail.com>
2025-06-22 22:33:38 +0900

    値判別周りを実装

--- QuizGame/MainWindow.xaml.cs
+++ QuizGame/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,48 @@ namespace QuizGame
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+
+            if (StartButton.Content.ToString() == "スタート")
+            {
+                StartButton.Content = "送信";
+                MethodButton.Content = "終了";
+                InputBox.Visibility = Visibility.Visible;
+                ModeStatus.Visibility = Visibility.Collapsed;
+                count = 0;
+            }
+            else if(StartButton.Content.ToString() == "送信")
+            {
+                string input = InputBox.Text;
+                if (int.TryParse(input, out int number))
+                {
+                    MessageBox.Show(number.ToString());
+                    History.Text += " " + number;
+                    InputBox.Clear();
+                    count++;
+                }
+                else
+                {
+                    MessageBox.Show("数字を入力してください");
+                    InputBox.Clear();
+                }
+            }
+        }
+
+        private void MethodButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (StartButton.Content.ToString() == "送信")
+            {
+                StartButton.Content = "スタート";
+                MethodButton.Content = "操作方法";
+                History.Text = "";
+                InputBox.Visibility = Visibility.Collapsed;
+                ModeStatus.Visibility = Visibility.Visible;
+            }
         }
     }
 }
