c2d089b40ebaacb2b9d92783f42d938040586ab7
tatsuki-s <zekoushn@gmail.com>
2025-06-24 21:01:39 +0900

    数値を判定できるようになった

--- QuizGame/MainWindow.xaml.cs
+++ QuizGame/MainWindow.xaml.cs
@@ -17,14 +17,26 @@ namespace QuizGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int randomNum;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void ResetGame()
+        {
+
+        }
+        private void StartGame()
+        {
+
+        }
+
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             int count = 0;
+            Random random = new Random();
 
             if (StartButton.Content.ToString() == "スタート")
             {
@@ -33,14 +45,34 @@ namespace QuizGame
                 InputBox.Visibility = Visibility.Visible;
                 ModeStatus.Visibility = Visibility.Collapsed;
                 count = 0;
+                randomNum = random.Next(1, 100);
+
+                //デバッグ用
+                MessageBox.Show(randomNum.ToString());
+
             }
             else if(StartButton.Content.ToString() == "送信")
             {
                 string input = InputBox.Text;
-                if (int.TryParse(input, out int number))
+                if(int.TryParse(input, out int inputNum))
                 {
-                    MessageBox.Show(number.ToString());
-                    History.Text += " " + number;
+                    MessageBox.Show(inputNum.ToString());
+
+                    if (inputNum == randomNum)
+                    {
+                        //MessageBox.Show("正解！");
+                        Check.Text = "正解";
+                        //StartButton.Content = "リスタート";
+                    }
+                    else if (inputNum >= randomNum)
+                    {
+                        Check.Text = "まだ大きい";
+                    }
+                    else
+                    {
+                        Check.Text = "まだ小さい";
+                    }
+                    History.Text += " " + inputNum;
                     InputBox.Clear();
                     count++;
                 }
@@ -54,13 +86,19 @@ namespace QuizGame
 
         private void MethodButton_Click(object sender, RoutedEventArgs e)
         {
+
             if (StartButton.Content.ToString() == "送信")
             {
+                if (Check.Text == "正解")
+                {
+                    StartButton.Content = "スタート";
+                }
                 StartButton.Content = "スタート";
                 MethodButton.Content = "操作方法";
                 History.Text = "";
                 InputBox.Visibility = Visibility.Collapsed;
                 ModeStatus.Visibility = Visibility.Visible;
+                Check.Text = "";
             }
         }
     }
